263f39ecfa84ce0de6403d9b192fbdcbb376f169
I-aM-FeNi <fenimabusa@gmail.com>
2023-06-12 15:46:03 -0700

    xD

--- 0x03-python-data_structures/100-test_lists.py
+++ 0x03-python-data_structures/100-test_lists.py
@@ -2,27 +2,20 @@ import ctypes
 
 lib = ctypes.CDLL('./libPyList.so')
 lib.print_python_list_info.argtypes = [ctypes.py_object]
-
-list1 = ['hello', 'World']
-lib.print_python_list_info(list1)
-
-del list1[1]
-lib.print_python_list_info(list1)
-
-list2 = list1 + [4, 5, 6.0, (9, 8), [9, 8, 1024], "Holberton"]
-lib.print_python_list_info(list2)
-
-list3 = []
-lib.print_python_list_info(list3)
-
-list3.append(0)
-lib.print_python_list_info(list3)
-
-list3.append(1)
-list3.append(2)
-list3.append(3)
-list3.append(4)
-lib.print_python_list_info(list3)
-
-list3.pop()
-lib.print_python_list_info(list3)
+l = ['hello', 'World']
+lib.print_python_list_info(l)
+del l[1]
+lib.print_python_list_info(l)
+l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "Holberton"]
+lib.print_python_list_info(l)
+l = []
+lib.print_python_list_info(l)
+l.append(0)
+lib.print_python_list_info(l)
+l.append(1)
+l.append(2)
+l.append(3)
+l.append(4)
+lib.print_python_list_info(l)
+l.pop()
+lib.print_python_list_info(l)
